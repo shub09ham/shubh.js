da41ceb6c78c983e9f243e4a056fadc2349cca5f
shub09ham <110715617+shub09ham@users.noreply.github.com>
2022-08-25 01:50:58 +0530

    Add files via upload

---
+++ positive integer.js
@@ -0,0 +1,5 @@
+let children=prompt("no of child")
+     let name= prompt("name")
+     let loc=prompt("loc")
+     let job=prompt("job")
+     document.write(`you will be a ${children} in ${name} location ${loc} and married to ${job}`);
